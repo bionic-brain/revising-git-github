3bf6868db0ab4e114f01184841814c591fd026f5
Shubham Kumar <tggskumar@gmail.com>
2023-05-05 11:01:23 +0530

    added 3rd variable in Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Sum{
     public static void main(String[] args){
         int a = 5;
         int b = 1;
-
-        System.out.println("Product : "+a *b);
+        int c = 2;
+        System.out.println("Product : "+a *b * c);
     }
 }
